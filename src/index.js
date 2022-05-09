1f0ecf35c7550ca83ed84231147a830858ab00e3
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2022-05-09 14:17:32 -0700

    Add infinite loop to `hello` method

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   switch (requestObject.method) {
     case 'hello':
-      return wallet.request({
-        method: 'snap_confirm',
-        params: [
-          {
-            prompt: `Hello, ${originString}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
-      });
+      while (true) {
+        // To infinity, and beyond.
+      };
     default:
       throw new Error('Method not found.');
   }
